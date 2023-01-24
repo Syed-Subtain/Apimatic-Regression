a129300f3ada978ff162677cffea00e32c43d608
Syed-Subtain <subtainshah2113@gmail.com>
2023-01-24 14:32:56 +0500

    Adding CI Test job

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {  
+    projectId: "juu3sa",
     baseUrl: 'https://app-apimaticio-test-eus.azurewebsites.net/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
